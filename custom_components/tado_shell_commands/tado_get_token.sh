e2848278c05266f022973608820bf238bcb00883
Sabsteef <38842670+sabsteef@users.noreply.github.com>
2020-02-09 22:32:42 +0100

    Update tado_get_token.sh

--- custom_components/tado_shell_commands/tado_get_token.sh
+++ custom_components/tado_shell_commands/tado_get_token.sh
@@ -1,16 +1,10 @@
-#get Token from Tado
+#get 
 
 
 # include parse_yaml function
 . /home/homeassistant/.homeassistant/custom_components/parse_yaml.sh
 
 eval $(parse_yaml /home/homeassistant/.homeassistant/secrets.yaml)
-
-printf 'the user is "%s"\n' $tado_mobile_user
-printf 'the password is "%s"\n' $tado_mobile_password
-printf 'the password is "%s"\n' $tado_client_secret
-printf 'the password is "%s"\n' $tado_cert_location
-
 
 curl -s "https://auth.tado.com/oauth/token" -d client_id=tado-web-app -d grant_type=password -d scope=home.user -d username=$tado_mobile_user -d password=$tado_mobile_password -d client_secret=$tado_client_secret  | jq -r '.access_token' > $tado_cert_location
 
