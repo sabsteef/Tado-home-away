4e866b448afc866f95ea4903f092fb15b1e01e04
Sabsteef <38842670+sabsteef@users.noreply.github.com>
2020-03-02 13:38:37 +0100

    Update tado_away.sh

--- custom_components/tado_shell_commands/tado_away.sh
+++ custom_components/tado_shell_commands/tado_away.sh
@@ -1,9 +1,19 @@
+#Set Tado to Away Mode
+
 # include parse_yaml function
 . /home/homeassistant/.homeassistant/custom_components/parse_yaml.sh
 
 # include variabled
 eval $(parse_yaml /home/homeassistant/.homeassistant/secrets.yaml)
 
+#generate token
+curl -s "https://auth.tado.com/oauth/token" -d client_id=tado-web-app -d grant_type=password -d scope=home.user -d username=$tado_mobile_user -d password=$tado_mobile_password -d client_secret=$tado_client_secret  | jq -r '.access_token' > $tado_cert_location
 
+#Set Geotracking to true ( I am away)
 curl -X PUT "https://my.tado.com/api/v2/homes/$tado_home/mobileDevices/$tado_mobile_device/settings" -H "Authorization: Bearer `cat $tado_cert_location`" -H "Content-Type:application/json;charset=UTF-8" --data-binary '{"geoTrackingEnabled":true}'
+
+# Debug option To validate if change is made on the Tado side
 #curl "https://my.tado.com/api/v2/homes/$tado_home/users" -H "Authorization: Bearer `cat $tado_cert_location`"
+
+# aditional steps to enable Home mode without Tado contract. ( can be disable if you have a contract.)
+python3 /home/homeassistant/.homeassistant/custom_components/tado_web.py
