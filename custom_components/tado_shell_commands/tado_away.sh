fa21842490d998a74b542269404055d062d2d38f
Sabsteef <38842670+sabsteef@users.noreply.github.com>
2020-03-05 22:07:46 +0100

    Update tado_away.sh

--- custom_components/tado_shell_commands/tado_away.sh
+++ custom_components/tado_shell_commands/tado_away.sh
@@ -1,7 +1,25 @@
 #Set Tado to Away Mode
+#!/bin/sh
 
 # include parse_yaml function
-. /opt/homeassistant/config/custom_components/tado_shell_commands/parse_yaml.sh
+#/opt/homeassistant/config/custom_components/tado_shell_commands/parse_yaml.sh
+
+function parse_yaml {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
 
 # include variabled
 eval $(parse_yaml /opt/homeassistant/config/secrets.yaml)
