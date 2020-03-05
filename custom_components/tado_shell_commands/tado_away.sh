10889aec482a265cbfd33e34d454a3bf730ec66c
Sabsteef <38842670+sabsteef@users.noreply.github.com>
2020-03-05 19:52:17 +0100

    Final Version

--- custom_components/tado_shell_commands/tado_away.sh
+++ custom_components/tado_shell_commands/tado_away.sh
@@ -1,10 +1,18 @@
 #Set Tado to Away Mode
 
 # include parse_yaml function
-. /home/homeassistant/.homeassistant/custom_components/parse_yaml.sh
+. /opt/homeassistant/config/custom_components/tado_shell_commands/parse_yaml.sh
 
 # include variabled
-eval $(parse_yaml /home/homeassistant/.homeassistant/secrets.yaml)
+eval $(parse_yaml /opt/homeassistant/config/secrets.yaml)
+
+# Debug option to validate if variables are extracted correctly from the secrets.yaml
+#printf 'the tado_mobile_user is "%s"\n' $tado_mobile_user
+#printf 'the tado_mobile_password is "%s"\n' $tado_mobile_password
+#printf 'the tado_client_secret is "%s"\n' $tado_client_secret
+#printf 'the tado_cert_location is "%s"\n' $tado_cert_location
+#printf 'the tado_home is "%s"\n' $tado_home
+#printf 'the tado_mobile_device is "%s"\n' $tado_mobile_device
 
 #generate token
 curl -s "https://auth.tado.com/oauth/token" -d client_id=tado-web-app -d grant_type=password -d scope=home.user -d username=$tado_mobile_user -d password=$tado_mobile_password -d client_secret=$tado_client_secret  | jq -r '.access_token' > $tado_cert_location
@@ -16,4 +24,4 @@ curl -X PUT "https://my.tado.com/api/v2/homes/$tado_home/mobileDevices/$tado_mob
 #curl "https://my.tado.com/api/v2/homes/$tado_home/users" -H "Authorization: Bearer `cat $tado_cert_location`"
 
 # aditional steps to enable Home mode without Tado contract. ( can be disable if you have a contract.)
-python3 /home/homeassistant/.homeassistant/custom_components/tado_web.py
+python3 /opt/homeassistant/config/custom_components/tado_shell_commands/tado_web.py $tado_mobile_user $tado_mobile_password
